7797056e13a98d6b84df34d09ceec4685ad588da
delver <98009770+delvering17@users.noreply.github.com>
2024-03-02 21:12:44 +0900

    Time: 1397 ms (47.83%), Space: 0B (100.00%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,14 @@
+SELECT
+    product_name,
+    SUM(o.unit) AS unit
+FROM
+    Products AS p
+LEFT JOIN
+    Orders AS o
+    ON p.product_id = o.product_id
+WHERE
+    o.order_date LIKE '2020-02%'
+GROUP BY
+    p.product_id
+HAVING
+    SUM(o.unit) >= 100
