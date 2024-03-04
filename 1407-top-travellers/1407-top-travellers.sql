2c487d5f4920c93eb3f8778d698dfbbdcf8e9e28
delver <98009770+delvering17@users.noreply.github.com>
2024-03-04 22:53:25 +0900

    Time: 1619 ms (45.89%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,13 @@
+SELECT
+    u.name,
+    IFNULL(SUM(distance), 0) AS travelled_distance
+FROM
+    Users AS u
+LEFT JOIN
+    Rides AS r
+    ON u.id = r.user_id
+GROUP BY
+    u.id
+ORDER BY
+    SUM(distance) DESC, u.name
+    
