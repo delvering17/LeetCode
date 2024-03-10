2c052524c954734c3d73f8de5de830a3db6a39ba
delver <98009770+delvering17@users.noreply.github.com>
2024-03-10 15:13:31 +0900

    Time: 828 ms (66.58%), Space: 0B (100.00%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,9 +1,14 @@
 SELECT
     activity_date AS day,
-    COUNT(DISTINCT user_id) AS active_users
-FROM
-    Activity
-WHERE
-    activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+    COUNT(user_id) AS active_users
+FROM (
+    SELECT DISTINCT
+        user_id,
+        activity_date
+    FROM
+        Activity
+    WHERE
+        activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+) AS subquery
 GROUP BY
-    activity_date
+    activity_date;
