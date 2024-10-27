3427f4e7c8903e36e534ba88a7bb3de3722f4894
delver <98009770+delvering17@users.noreply.github.com>
2024-10-27 15:54:46 +0900

    Time: 14 ms (32.12%), Space: 44.9 MB (30.08%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String filteredStr = s
+            .replaceAll("[^a-zA-Z0-9]", "")
+            .toLowerCase();
+        
+        String reversedStr = new StringBuilder(filteredStr)
+            .reverse()
+            .toString();
+        
+        return reversedStr.equals(filteredStr); 
+    }
+}
