390fa6a756823e6f179ba15d6e53b1b6efa8d76d
Chanwook Song <98009770+delvering17@users.noreply.github.com>
2025-05-30 15:59:26 +0900

    Time: 1966 ms (5.02%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -4,16 +4,14 @@ SELECT
 FROM 
     Product AS p
 WHERE
-    NOT EXISTS(
+    EXISTS(
         SELECT 1
         FROM Sales AS s
         WHERE p.product_id = s.product_id
-        AND s.sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
-    )
-    AND EXISTS(
-        SELECT 1
-        FROM Sales AS s
-        WHERE p.product_id = s.product_id
-        AND s.sale_date BETWEEN '2019-01-01' AND '2019-03-31'
+        GROUP BY
+            s.product_id
+        HAVING
+        MIN(s.sale_date) >= '2019-01-01'
+        AND MAX(s.sale_date) <= '2019-03-31'
     )
     
